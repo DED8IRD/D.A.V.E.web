e08d5f13d072d556e1bdf140ef90def0ed6b2dcc
DED8IRD <ewu72829@gmail.com>
2019-09-03 04:32:05 -0700

    client connects with full screenplay api (slow)

--- client/src/components/SourceForm.tsx
+++ client/src/components/SourceForm.tsx
@@ -13,24 +13,53 @@ import {
   Chip
 } from '@material-ui/core'
 
+interface Film {
+  title: string,
+  path: string,
+  genre: string
+}
+
+interface Genre {
+  genre: string,
+  films: Film[]
+}
+
 const SourceForm: React.FC = (props: any) => {
-  const [films, setFilms] = React.useState([])
-  React.useEffect(() => {
-    if (films.length === 0) {
-      (async () => {
-        try {
-          const response = await axios.get('http://localhost:8000/api/sources/')
-          setFilms(response.data)
-        } catch (err) {
-          console.log(err)
-        }
-      })()
+  const [films, setFilms] = React.useState<Genre[]>([])
+  const getFilms = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/sources/')
+      setFilms(response.data)
+    } catch (err) {
+      console.log(err)
     }
+  }
+  React.useEffect(() => {
+    if (films.length === 0) getFilms()
   })
 
   return (
     <React.Fragment>
-        
+        {films.map(genre => {
+          console.log(genre)
+          return (
+            <FormControl>
+              <p>{genre.genre}</p>
+              {
+                genre.films.map(film => {
+                  return (
+                    <FormControlLabel
+                      control={
+                        <Checkbox value="checkedA" />
+                      }
+                      label={film.title}
+                    />          
+                  )
+                })
+              }
+            </FormControl>
+          )
+        })}
     </React.Fragment>
   )
 }
