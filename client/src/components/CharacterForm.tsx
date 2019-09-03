627ac4ebe95de35af73b8a9f315c8c5804a85d3b
DED8IRD <ewu72829@gmail.com>
2019-09-03 01:36:08 -0700

    working local character form without shared state

--- client/src/components/CharacterForm.tsx
+++ client/src/components/CharacterForm.tsx
@@ -1,5 +1,6 @@
 // CharacterForm.tsx
 import React, {
+  useState,
   ChangeEvent,
   FormEvent,
 } from 'react';
@@ -13,16 +14,16 @@ import {
   Paper
 } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
-import DeleteIcon from '@material-ui/icons/Delete';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
+    paper: {
       display: 'flex',
       justifyContent: 'center',
       flexWrap: 'wrap',
       padding: theme.spacing(0.5),
+      margin: theme.spacing(3)
     },
     chip: {
       margin: theme.spacing(0.5),
@@ -30,38 +31,36 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-interface CharacterChip {
-  key: number;
-  label: string;
-}
-
 interface CharacterChipProps {
-  chipList: CharacterChip[], 
-  handleDelete: (chipsToDelete: CharacterChip) => any
+  chips: string[], 
+  handleDelete: (index: number) => any
 }
 
-const CharacterChips = ({chipList, handleDelete}: CharacterChipProps) => {
+const CharacterChipArray = ({chips, handleDelete}: CharacterChipProps) => {
+  const classes = useStyles();
   return (
-    <Paper>
-      {chipList.map((chip: CharacterChip) => (
+    <Paper className={classes.paper}>
+      {chips.map((chip: string, i: number) => {
+        return (
           <Chip
-            key={chip.key}
-            label={chip.label}
-            onDelete={handleDelete(chip)}
+            key={i}
+            label={chip}
+            onDelete={handleDelete(i)}
+            className={classes.chip}
           />
-        )
-      )}
+        );
+      })}
     </Paper>
   );
 }
 
 const CharacterForm: React.FC = () => {
-  const [chips, setChips] = React.useState<CharacterChip[]>([
-    {key: 0, label: 'test'},
-    {key: 1, label: 'one'},
-    {key: 2, label: 'two'},
-  ]);
-  const [character, setCharacter] = React.useState<string>('');
+  const [chips, setChips] = React.useState<string[]>([])
+  const [character, setCharacter] = useState<string>('')
+
+  const handleDelete = (index: number) => () => {
+    setChips(chips.filter((chip: string, i: number) => i !== index))
+  };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setCharacter(event.currentTarget.value)
@@ -69,45 +68,40 @@ const CharacterForm: React.FC = () => {
 
   const handleAdd = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault()
-    const chip: CharacterChip = {key: chips.length, label: character}
-    console.log(chips, chip, [...chips, chip])
-    setChips(chipList => [...chipList, chip])
-    console.log(chips)
+    setChips([...chips, character])
     setCharacter('')
   }
-
-  const handleDelete = (chipToDelete: CharacterChip): void => {
-    setChips(chips => chips.filter(chip => chip.key !== chipToDelete.key));
-  };
 
   return (
     <React.Fragment>
       <Typography variant="h5" align="center" gutterBottom>
         Add characters
       </Typography>
-        <form onSubmit={handleAdd}>     
-            <TextField 
-              id="character" 
-              name='character'
-              label="Characters" 
-              value={character}
-              onChange={handleChange}
-              fullWidth
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton type='submit' aria-label="add">
-                      <AddIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}              
-            />
-        </form>
-        <CharacterChips 
-          chipList={chips}
+      <form onSubmit={handleAdd}>     
+        <TextField 
+          id="character" 
+          name='character'
+          label="Characters" 
+          value={character}
+          onChange={handleChange}
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton type='submit' aria-label="add">
+                  <AddIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}              
+        />
+      </form>
+      {chips.length > 0 && 
+        <CharacterChipArray 
+          chips={chips}
           handleDelete={handleDelete}
         />
+      }
     </React.Fragment>
   )
 }
