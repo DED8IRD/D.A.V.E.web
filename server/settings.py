6bbf68f0d50f0333a24dff1eea72ba27a1b456fb
DED8IRD <ewu72829@gmail.com>
2019-09-08 14:16:53 -0700

    config media root

--- server/settings.py
+++ server/settings.py
@@ -118,12 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'client', 'build', 'static'),
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # CORS whitelist
 CORS_ORIGIN_WHITELIST = (
